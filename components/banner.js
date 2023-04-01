dbba5e40ce3f063989000aad99ce7be7ed31da6a
Pirathees <kamalanathanpirathees@gmail.com>
2023-04-02 03:00:05 +0530

    Images added

--- components/banner.js
+++ components/banner.js
@@ -6,6 +6,7 @@ const Banner =(props)=>{
     return (<div className={styles.container}>
                 <h1 className={styles.title}> <span className={styles.title1}>FoodShop</span> <span className={styles.title2}>Finder</span> </h1>
                 <p className={styles.subTitle}> Discover your local food shops</p>
+                <br></br>
                 <button onClick={props.handleOnClick} className={styles.button} > {props.buttonText} </button>
             </div>)
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client';
-import Image from 'next/image'
+import Image from 'next/image' //Image component do auto scalling -> helps in performance optimization as well as only loads image on demand
 import styles from './page.module.css'
 import Banner from '../../components/banner'
 
@@ -14,6 +14,9 @@ export default function Home() {
     <div className={styles.main}>
       <Banner buttonText="View Shops" handleOnClick={handleOnBannerBtnClick}></Banner>
       Home Page
+      <Image className={styles.heroImage} src="/static/hero-image.png" width={700} height={400}></Image>
     </div>
+
+    
   )
 }
